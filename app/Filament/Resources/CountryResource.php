6ff4bd24057f7c962261707b70f6cef22e04c6e6
Marcos CDV <yo.mark406@gmail.com>
2024-07-21 18:09:51 -0300

    filament loggin, world seeder & crud w/filament

---
+++ app/Filament/Resources/CountryResource.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CountryResource\Pages;
+use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Models\Country;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\TextInput::make('iso2')
+                    ->required()
+                    ->maxLength(2),
+                Forms\Components\TextInput::make('iso3')
+                    ->required()
+                    ->maxLength(3),
+                Forms\Components\TextInput::make('numeric_code')
+                    ->maxLength(3),
+                Forms\Components\TextInput::make('phonecode')
+                    ->tel()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('capital')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('currency')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('currency_name')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('currency_symbol')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('tld')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('native')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('region')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('subregion')
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('timezones')
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('translations')
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('latitude')
+                    ->numeric(),
+                Forms\Components\TextInput::make('longitude')
+                    ->numeric(),
+                Forms\Components\TextInput::make('emoji')
+                    ->maxLength(191),
+                Forms\Components\TextInput::make('emojiU')
+                    ->maxLength(191),
+                Forms\Components\Toggle::make('flag')
+                    ->required(),
+                Forms\Components\Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('iso2')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('iso3')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('numeric_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phonecode')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('capital')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('currency')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('currency_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('currency_symbol')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tld')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('native')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('region')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subregion')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('latitude')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('longitude')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('emoji')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('emojiU')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('flag')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCountries::route('/'),
+            'create' => Pages\CreateCountry::route('/create'),
+            'edit' => Pages\EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
