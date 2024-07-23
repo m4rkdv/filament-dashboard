2b01f8fa5bdbf5ae4a771cf4393ce570b2379bf4
Marcos CDV <yo.mark406@gmail.com>
2024-07-22 23:58:42 -0300

    database users update schema

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -16,7 +16,7 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
+    protected static ?string $navigationLabel ="Employees";
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
