384df07e8d04702ceb3169866ac70913d9846f42
Marcos CDV <yo.mark406@gmail.com>
2024-07-28 23:39:58 -0300

    timesheet format table and filters

--- app/Filament/Resources/TimesheetResource.php
+++ app/Filament/Resources/TimesheetResource.php
@@ -12,6 +12,9 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 
 class TimesheetResource extends Resource
 {
@@ -23,7 +26,22 @@ class TimesheetResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Select::make('calendar_id')
+                    ->relationship(name: 'calendar', titleAttribute: 'name')
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship(name: 'user', titleAttribute: 'name')
+                    ->required(),
+                Forms\Components\Select::make('type')
+                    ->options([
+                        'work' => 'Working',
+                        'pause' => 'in Pause',
+                    ])
+                    ->required(),
+                Forms\Components\DateTimePicker::make('day_in')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('day_out')
+                    ->required(),
             ]);
     }
 
@@ -31,10 +49,34 @@ class TimesheetResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('calendar.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('day_in')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('day_out')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
             ])
             ->filters([
-                //
+                SelectFilter::make('type')
+                    ->options([
+                        'work' => 'Working',
+                        'pause' => 'in Pause',
+                    ])
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
