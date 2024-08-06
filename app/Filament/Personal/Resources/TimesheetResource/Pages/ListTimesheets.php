038a85d79bcadfff5122827839325d6c5bf8322a
Marcos CDV <yo.mark406@gmail.com>
2024-08-05 23:00:47 -0300

    Timesheet buttons for work and break

--- app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
+++ app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Personal\Resources\TimesheetResource\Pages;
 
-use App\Filament\Personal\Resources\TimesheetResource;
+use Carbon\Carbon;
 use Filament\Actions;
+use App\Models\Timesheet;
+use Filament\Actions\Action;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Personal\Resources\TimesheetResource;
 
 class ListTimesheets extends ListRecords
 {
@@ -14,6 +18,26 @@ class ListTimesheets extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Action::make('inWork')
+                ->label('Start Work')
+                ->color('success')
+                ->keyBindings(['command+s','ctrl+s'])
+                ->action(function(){
+                    $user= Auth::user();
+                    $timesheet= new Timesheet();
+                    $timesheet->calendar_id=1;
+                    $timesheet->user_id=$user->id;
+                    $timesheet->day_in=Carbon::now();
+                    $timesheet->day_out=Carbon::now();
+                    $timesheet->type='work';
+                    $timesheet->save();
+                })
+                ->requiresConfirmation(),
+            Action::make('inPause')
+                ->label('Start Breake')
+                ->color('info')
+                ->requiresConfirmation(),
+            
         ];
     }
 }
